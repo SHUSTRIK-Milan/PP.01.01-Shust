66d6e6e0e5ae9830ca39e5b2a3cbd0a42ecb9039
SHUSTRIK <64786318+SHUSTRIK-Milan@users.noreply.github.com>
2025-06-06 23:17:13 +0600

    3 и 4 контрольные точки

---
+++ FoodApp/app/src/main/java/com/example/foodapp/welcome1.java
@@ -0,0 +1,32 @@
+package com.example.foodapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class welcome1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler().postDelayed(() -> {
+            Intent welcome2 = new Intent(getApplicationContext(), welcome2.class);
+            startActivity(welcome2);
+            finish();
+        }, 2000);
+    }
+}
